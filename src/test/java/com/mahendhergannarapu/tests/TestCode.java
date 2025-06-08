3d26cba01c88d2f4ee246e02fad650fe3cbc8a35
Mahendher-Gannarapu <gannarapu.mahendher99@gmail.com>
2025-06-08 22:27:07 +0530

    Add Files

--- src/test/java/com/mahendhergannarapu/tests/TestCode.java
+++ src/test/java/com/mahendhergannarapu/tests/TestCode.java
@@ -1,4 +1,16 @@
 package com.mahendhergannarapu.tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class TestCode {
+
+    @Test
+    public void test()
+    {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://app.vwo.com");
+        driver.quit();
+    }
 }
